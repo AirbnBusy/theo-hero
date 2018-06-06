8da774fb70ce1056cb1a7230a3bc3269339ce982
theo <theospielman@gmail.com>
2018-06-05 23:42:55 -0700

    Change variable name from images to imageURIs for clarity/accuracy

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.resolve(__dirname, '../public/')));
 app.get('/api/listings/:listingID/photos', (req, res) => {
   const { listingID } = req.params;
   selectImagesByListingId(listingID)
-    .then(images => res.status(200).send(images))
+    .then(imageURIs => res.status(200).send(imageURIs))
     .catch(err => res.status(500).send(err.message));
 });
 
