28ef49ffb83c26ac4dc1977d63937a92583f77c0
theo <theospielman@gmail.com>
2018-06-05 23:38:40 -0700

    Create DB connection, functions, and API route

--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+
 const app = express();
 const path = require('path');
+const { selectImagesByListingId } = require('../database/db-func');
 
 app.use(express.static(path.resolve(__dirname, '../public/')));
 
+app.get('/api/listings/:listingID/photos', (req, res) => {
+  const { listingID } = req.params;
+  selectImagesByListingId(listingID)
+    .then(images => res.status(200).send(images))
+    .catch(err => res.status(500).send(err.message));
+});
 
 app.listen(3003, () => {
   console.log('listening on port 3003');
 });
+
+module.exports = app;
