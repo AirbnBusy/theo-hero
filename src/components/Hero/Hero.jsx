6bd6dad228a7a6c0d8f1ac2a0fb4d60c0dc46ced
theo <theospielman@gmail.com>
2018-06-07 20:04:55 -0700

    Begin implementing hero image and buttons

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,26 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import Button from '../Button/Button';
+import styles from './Hero.css';
 
-class Hero extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="hero-container">
-        <h1> Hero Component </h1>
-        <div className="upper-buttons">
-          <Button />
-          <Button />
-        </div>
-        <div className="lower-button">
-          <Button />
-        </div>
+const Hero = ({ testImgSrc }) =>
+  (
+    <div className={styles.container} style={{ backgroundImage: `url(${testImgSrc})` }}>
+      <div className={styles['upper-button-box']}>
+        <Button />
+        <Button />
       </div>
-    );
-  }
-}
+      <div className={styles['lower-button-box']}>
+        <Button />
+      </div>
+    </div>
+  );
+
+Hero.propTypes = {
+  testImgSrc: PropTypes.string,
+};
+
+Hero.defaultProps = {
+  testImgSrc: 'https://s3-us-west-1.amazonaws.com/airbnbusy/photos/p00022.jpg',
+};
 
 export default Hero;
