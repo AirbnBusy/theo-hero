0c3b27d6d4062aed78915863d22f1dd530c48213
theo <theospielman@gmail.com>
2018-06-16 10:20:38 -0700

    Restyle share/save svgs for chrome compatability

--- database/db-func.js
+++ database/db-func.js
@@ -4,7 +4,7 @@ const bluebird = require('bluebird');
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  // password: 'your password',
+  password: 'loot',
   database: 'hero_module',
 });
 
